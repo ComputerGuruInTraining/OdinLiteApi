d8c95d53d0f16da6d69ecbc83b0dfb9e630bbb14
Bernie Carroll <bernadettecar77@hotmail.com>
2018-01-30 22:56:45 +1100

    odinliteapi url

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://odinliteapitest.azurewebsites.net'),
+    'url' => env('APP_URL', 'https://odinliteapi.azurewebsites.net'),
 
     /*
     |--------------------------------------------------------------------------
