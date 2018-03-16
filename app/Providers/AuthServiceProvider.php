e883a6a0c9cf61d570b446bc4e8309cb18055d7a
Bernie Carroll <bernadettecar77@hotmail.com>
2018-03-16 13:13:03 +1100

    Expire and route

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //configure short lived tokens
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(2));
     }
 }
